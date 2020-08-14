22a561eab210b0ba4bbb336558742e3754fc9ed4
LetsDream <49693336+LetsDreamcc@users.noreply.github.com>
2020-08-14 02:42:26 +0200

    Add files via upload

---
+++ Basic DiscordBot/Module/commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Discordbot.Module
+{
+    public class commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("command")]
+        public async Task command()
+        {
+            await ReplyAsync("Message");
+            Console.WriteLine(DateTime.Now + " Requested: -command");
+        }
+    }
+}
---
+++ Basic DiscordBot/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Discordbot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = ""; //Bot Token
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+            await RegisterReactionsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+
+            if (message.Content.Contains("Word"))
+            {
+                await context.Channel.SendMessageAsync("I don't like that Word!");
+                await context.Message.DeleteAsync();
+            }
+        }
+
+        public async Task RegisterReactionsAsync()
+        {
+            _client.ReactionAdded += HandleReactionsAsync;
+            await Task.Delay(1);
+        }
+
+        private async Task HandleReactionsAsync(Cacheable<IUserMessage, ulong> before, ISocketMessageChannel channel, SocketReaction reaction)
+        {
+            await Task.Delay(1);
+        }
+    }
+}
